0f70401cc323fdf089a3ee3c1c90891e4ec7977b
cosc19111119 <115706969+cosc19111119@users.noreply.github.com>
2024-12-05 20:04:22 +0500

    check

--- src/Components/Projects/CheckBoxWIthUploadDelete.tsx
+++ src/Components/Projects/CheckBoxWIthUploadDelete.tsx
@@ -1,33 +1,63 @@
-
-'use client'
-import React, { useState } from 'react'
-import { DeleteIcon2, Uploadicon } from '../utils/SvgIcons'
+"use client";
+import React, { useState } from "react";
+import { DeleteIcon2, Uploadicon } from "../utils/SvgIcons";
 
 const CheckBoxWIthUploadDelete = () => {
   const [isChecked, setIsChecked] = useState(false);
+
   return (
     <div className="mt-6 no-scrollbar flex h-[44px] items-center justify-between">
-    <div className="flex cursor-pointer items-center gap-3 font-medium text-base">
-      <span className="group inline-flex focus:outline-none">
-      <div
-      className="relative isolate flex items-center justify-center rounded-[0.3125rem] 
-        border border-white/85 border-solid
-        w-5 h-5"
-      onClick={() => setIsChecked(!isChecked)}
-      style={{
-        backgroundColor: isChecked ? "#524cd5" : "transparent", 
-      }}
-    >
-      
-    </div>
-      </span>
-      Total 2 videos
-    </div>
-    <div className="flex gap-4">
-      <button
-        data-tooltip-id="tooltip"
-        data-tooltip-content="Only rendered videos can be exported"
-        className="text-white bg-transparent border-[1px] border-solid shadow-md 
+      <div className="flex cursor-pointer items-center gap-3 font-medium text-base">
+        <span className="group inline-flex focus:outline-none">
+          <div
+            className="relative isolate flex items-center justify-center rounded-[0.3125rem] 
+              border border-white/85 border-solid
+              w-5 h-5"
+            onClick={() => setIsChecked(!isChecked)}
+            style={{
+              backgroundColor: isChecked ? "#524cd5" : "transparent",
+            }}
+          >
+            {/* Checkmark Icon */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              width="14"
+              height="14"
+              className={`absolute ${
+                isChecked ? "opacity-100" : "opacity-0"
+              } transition-opacity`}
+            >
+              <path
+                fillRule="evenodd"
+                d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              width="14"
+              height="14"
+              className={`absolute opacity-0 transition-opacity`}
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.25 12a.75.75 0 0 1 .75-.75h14a.75.75 0 0 1 0 1.5H5a.75.75 0 0 1-.75-.75Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+        </span>
+        Total 2 videos
+      </div>
+      <div className="flex gap-4">
+        <button
+          data-tooltip-id="tooltip"
+          data-tooltip-content="Only rendered videos can be exported"
+          className="text-white bg-transparent border-[1px] border-solid shadow-md 
     
      relative isolate inline-flex items-center justify-center 
     gap-x-2 text-base/6 font-sans hover:opacity-80 
@@ -38,19 +68,19 @@ const CheckBoxWIthUploadDelete = () => {
     focus:outline-none px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] 
     sm:px-[calc(theme(spacing.3)-1px)] sm:py-[calc(theme(spacing[1.5])-1px)] 
     sm:text-sm/6 rounded border-white/20 font-bold cursor-not-allowed"
-        type="button"
-        disabled
-      >
-       <Uploadicon/>
-        Upload
-        <span
-          className="-translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2 size-[max(100%,2.75rem)] [@media(pointer:fine)]:hidden"
-          aria-hidden="true"
-        />
-      </button>
-      <button
-        data-tooltip-id="tooltip"
-        className="text-white bg-transparent border-[1px] border-solid shadow-md 
+          type="button"
+          disabled
+        >
+          <Uploadicon />
+          Upload
+          <span
+            className="-translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2 size-[max(100%,2.75rem)] [@media(pointer:fine)]:hidden"
+            aria-hidden="true"
+          />
+        </button>
+        <button
+          data-tooltip-id="tooltip"
+          className="text-white bg-transparent border-[1px] border-solid shadow-md 
     [--btn-icon:theme(colors.white)] data-[active]:[--btn-icon:theme(colors.white)] 
     data-[hover]:[--btn-icon:theme(colors.white)] relative isolate inline-flex items-center 
     justify-center gap-x-2 text-base/6 font-sans hover:opacity-80 data-[disabled]:opacity-50 
@@ -58,17 +88,14 @@ const CheckBoxWIthUploadDelete = () => {
     focus:outline-none px-[calc(theme(spacing[3.5])-1px)] py-[calc(theme(spacing[2.5])-1px)] 
     sm:px-[calc(theme(spacing.3)-1px)] sm:py-[calc(theme(spacing[1.5])-1px)] sm:text-sm/6 
     rounded border-white/20 font-bold cursor-pointer"
-        type="button"
-      >
-       < DeleteIcon2/>
-        <span
-          className="-translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2 size-[max(100%,2.75rem)] [@media(pointer:fine)]:hidden"
-         
-        />
-      </button>
+          type="button"
+        >
+          <DeleteIcon2 />
+          <span className="-translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2 size-[max(100%,2.75rem)] [@media(pointer:fine)]:hidden" />
+        </button>
+      </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default CheckBoxWIthUploadDelete
+export default CheckBoxWIthUploadDelete;
--- src/Components/Projects/ProjectCard.tsx
+++ src/Components/Projects/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ThreeDotIonP } from "../utils/SvgIcons";
 import Link from "next/link";
 
@@ -7,7 +7,7 @@ interface ProjectCardProps {
   thumbnailUrl: string;
   title: string;
   editedDate: string;
-  onOptionsClick?: () => void; 
+  onOptionsClick?: () => void;
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({
@@ -17,6 +17,8 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   editedDate,
   onOptionsClick,
 }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
     <Link href={"/projects/detail"} className="w-full relative group">
       <div className="group relative">
@@ -24,12 +26,35 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
           data-slot="control"
           className="group-hover:visible invisible absolute top-2 left-2 z-30 group inline-flex focus:outline-none"
           role="checkbox"
-          aria-checked="false"
-          tabIndex={0}
-          data-headlessui-state=""
         >
-          <div className="relative isolate flex items-center justify-center rounded-[0.3125rem] border border-white/85 border-solid w-6 h-6"></div>
+          <div
+            className={`relative isolate flex items-center justify-center rounded-[0.3125rem] border border-white/85 border-solid w-6 h-6 ${
+              isChecked ? "bg-[#6c6cf5]" : "bg-transparent"
+            }`}
+            onClick={(e) => {
+              e.preventDefault(); // Prevent the link click
+              setIsChecked((prev) => !prev);
+            }}
+          >
+            {/* Checkmark Icon */}
+            {isChecked && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                width="14"
+                height="14"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M19.916 4.626a.75.75 0 0 1 .208 1.04l-9 13.5a.75.75 0 0 1-1.154.114l-6-6a.75.75 0 0 1 1.06-1.06l5.353 5.353 8.493-12.74a.75.75 0 0 1 1.04-.207Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            )}
+          </div>
         </span>
+        <div className="absolute border-[2px] border-[#6c6cf5] flex top-0 left-0 z-10 h-full w-full flex-col items-center gap-y-2 rounded-2xl  opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
         <div className="flex-col hover:shadow-focus-md fade-in animate-in cursor-pointer overflow-hidden rounded-2xl bg-area-container duration-200">
           <div className="relative aspect-[7/4] max-w-full">
             <div className="h-full w-full">
@@ -88,9 +113,6 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
             </div>
           </div>
         </div>
-      </div>
-      <div className="absolute border-[2px] border-[#6c6cf5] flex top-0 left-0 z-10 h-full w-full flex-col items-center gap-y-2 rounded-2xl  opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-     
       </div>
     </Link>
   );
